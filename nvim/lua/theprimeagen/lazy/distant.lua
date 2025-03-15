6b6da5e4f84c3cedf516bb9b4a49ff05f90533dd
brunovskyoliver <obrunovsky7@gmail.com>
2025-03-15 20:42:34 +0100

    automat push

--- nvim/lua/theprimeagen/lazy/distant.lua
+++ nvim/lua/theprimeagen/lazy/distant.lua
@@ -19,3 +19,33 @@ return {
         require('distant'):setup()
     end
 }
+local telescope = require('telescope')
+local distant = require('distant')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+-- Custom function to find files on a remote host
+local function remote_find_files(opts)
+  opts = opts or {}
+  telescope.pickers.new(opts, {
+    prompt_title = 'Remote Files',
+    finder = telescope.finders.new_table {
+      results = distant.files.list({ path = opts.path or '.' }),
+    },
+    sorter = telescope.config.values.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        if selection then
+          vim.cmd('edit ' .. selection.value)
+        end
+      end)
+      return true
+    end,
+  }):find()
+end
+
+-- Keybinding to trigger the remote file finder
+vim.api.nvim_set_keymap('n', '<leader>rf', '<cmd>lua remote_find_files()<CR>', { noremap = true, silent = true })
+
