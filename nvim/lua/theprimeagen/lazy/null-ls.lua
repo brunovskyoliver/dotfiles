2b5f87176e49a2d76dea1185afa846913385b583
brunovskyoliver <obrunovsky7@gmail.com>
2025-03-16 09:41:30 +0100

    automat push

--- nvim/lua/theprimeagen/lazy/null-ls.lua
+++ nvim/lua/theprimeagen/lazy/null-ls.lua
@@ -4,6 +4,11 @@ return {
     dependencies = { "williamboman/mason.nvim", "jay-babu/mason-null-ls.nvim" },
     config = function()
         local null_ls = require("null-ls")
+        require("mason-null-ls").setup({
+            ensure_installed = { "xmlformatter" }, -- Ensures xmlformatter is installed
+            automatic_installation = true,
+        })
+
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.xmlformat,
