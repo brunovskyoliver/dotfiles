7eca149ea4c38c7111b3bfba16f9c60da1e80ce2
brunovskyoliver <obrunovsky7@gmail.com>
2025-03-22 18:16:14 +0100

    automat push

--- nvim/lua/theprimeagen/lazy/lualine.lua
+++ nvim/lua/theprimeagen/lazy/lualine.lua
@@ -1,38 +1,42 @@
 return {
-    require('lualine').setup() {
-        options = {
-            theme = 'gruvbox',
-            section_separators = { '', '' },
-            component_separators = { '', '' },
-        },
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup() {
+            options = {
+                theme = 'gruvbox',
+                section_separators = { '', '' },
+                component_separators = { '', '' },
+            },
 
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch' },
-            lualine_c = { 'filename' },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' },
-        },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
+            },
 
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {},
-        },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {},
+            },
 
-        tabline = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = {},
-        },
+            tabline = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
 
-        extensions = { 'fugitive', 'nvim-tree' },
-    }
+            extensions = { 'fugitive', 'nvim-tree' },
+        }
+    end,
 }
