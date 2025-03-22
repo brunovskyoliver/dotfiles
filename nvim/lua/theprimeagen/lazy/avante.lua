c9f24b0c67fccc5accae6c8dea4293b8bcf80233
brunovskyoliver <obrunovsky7@gmail.com>
2025-03-22 20:29:34 +0100

    automat push

--- nvim/lua/theprimeagen/lazy/avante.lua
+++ nvim/lua/theprimeagen/lazy/avante.lua
@@ -20,7 +20,7 @@ return {
                 __inherited_from = "openai",
                 api_key_name = "GROQ_API_KEY",
                 endpoint = "https://api.groq.com/openai/v1/",
-                model = "llama-3.1-70b-versatile",
+                model = "llama3-70b-8192",
             },
         },
     },
