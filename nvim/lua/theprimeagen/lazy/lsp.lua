cc4b966fc40fe4795a5400ea15eca1c76c2f89e8
brunovskyoliver <obrunovsky7@gmail.com>
2025-04-03 21:00:52 +0200

    automat push

--- nvim/lua/theprimeagen/lazy/lsp.lua
+++ nvim/lua/theprimeagen/lazy/lsp.lua
@@ -68,6 +68,7 @@ return {
 
                     lspconfig.omnisharp.setup({
                         cmd = {
+                            "mono",
                             "/Applications/Unity/Hub/Editor/6000.0.43f1/Unity.app/Contents/MonoBleedingEdge/bin/mono",
                             "/Users/oliver/bin/omnisharp-mono/OmniSharp.exe",
                             "--languageserver",
