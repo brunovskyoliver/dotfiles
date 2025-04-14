4f9d9647355157c06a7139bffe8e78b6544a2118
brunovskyoliver <obrunovsky7@gmail.com>
2025-04-14 17:29:33 +0200

    automat push

--- nvim/lua/theprimeagen/lazy/harpoon.lua
+++ nvim/lua/theprimeagen/lazy/harpoon.lua
@@ -5,10 +5,10 @@ return {
         local harpoon = require "harpoon"
         harpoon:setup()
 
-        vim.keymap.set("n", "<m-h><m-m>", function()
+        vim.keymap.set("n", "<A-h><A-m>", function()
             harpoon:list():add()
         end)
-        vim.keymap.set("n", "<m-h><m-l>", function()
+        vim.keymap.set("n", "<A-h><A-l>", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end)
 
