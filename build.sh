90cb567a89e50d6fd24207dbdaf68a01f09bded8
brunovskyoliver <obrunovsky7@gmail.com>
2025-04-15 18:55:51 +0200

    automat push

--- build.sh
+++ build.sh
@@ -22,4 +22,6 @@ for file in $files; do
     $cmd "$file" "$HOME"
 done
 
-(cd ~/dotfiles && git add . && git commit -m "automat push" && git push origin main --force) >/dev/null 2>&1 &
+if [[ "$1" == "-d" ]]; then
+    (cd ~/dotfiles && git add . && git commit -m "automat push" && git push origin main --force) >/dev/null 2>&1 &
+fi
