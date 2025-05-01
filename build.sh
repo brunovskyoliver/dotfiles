f90e99e1bd5b7756d58a38269ef1b760ff293dd2
brunovskyoliver <obrunovsky7@gmail.com>
2025-05-01 20:14:55 +0200

    automat push

--- build.sh
+++ build.sh
@@ -25,3 +25,16 @@ done
 if [[ "$1" == "-d" ]]; then
     (cd ~/dotfiles && git add . && git commit -m "automat push" && git push origin main --force) >/dev/null 2>&1 &
 fi
+
+source /etc/os-release
+
+if [[ "$ID" == "arch" ]]; then
+    sudo pacman -S --needed --noconfirm base-devel openssl zlib readline gnu-netcat openssh pipewire wireplumber xdg-desktop-portal-hyprland xdg-desktop-portal-gtk networkmanager mesa vulkan-icd-loader
+    systemctl enable NetworkManager
+    systemctl start NetworkManager
+    sudo pacman -S hyprland waybar dunst rofi kitty thunar firefox neovim qt5-wayland qt6-wayland
+    sudo pacman -S polkit-gnome network-manager-applet blueman pavucontrol brightnessctl wofi swaylock swayidle
+    curl https://pyenv.run | bash
+    exec zsh
+    sudo pacman -S zoxide
+fi
