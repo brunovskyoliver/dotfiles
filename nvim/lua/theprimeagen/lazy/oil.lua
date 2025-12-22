dbcc0ab3184b61a5473a720a0d200b2d42bad464
brunovskyoliver <obrunovsky7@gmail.com>
2025-12-22 22:25:07 +0100

    automat push

--- nvim/lua/theprimeagen/lazy/oil.lua
+++ nvim/lua/theprimeagen/lazy/oil.lua
@@ -1,4 +1,5 @@
 return {
+'stevearc/oil.nvim',
 require("oil").setup({
   -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
   -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
