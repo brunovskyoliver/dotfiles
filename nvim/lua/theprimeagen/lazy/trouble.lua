23beae2e77ef5b1af750ae31b9b4d57eb867f781
brunovskyoliver <obrunovsky7@gmail.com>
2025-12-22 22:18:12 +0100

    automat push

--- nvim/lua/theprimeagen/lazy/trouble.lua
+++ nvim/lua/theprimeagen/lazy/trouble.lua
@@ -3,13 +3,13 @@ return {
   opts = {},
   cmd = "Trouble",
   keys = {
+    -- {
+    --   "<leader>tt",
+    --   "<cmd>Trouble diagnostics toggle<cr>",
+    --   desc = "Diagnostics (Trouble)",
+    -- },
     {
       "<leader>tt",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>tT",
       "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
       desc = "Buffer Diagnostics (Trouble)",
     },
